672ff2eabf69a8a54ee8547f6d8a4ee28b1fcf4b
Ariions <arnas.bendinskas@gmail.com>
2020-04-02 12:47:10 +0300

    Tiles?
    tired implimenting tiles but i think this will take a bit too much time its better to just use physics?

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum direction
+{
+    north = 1,
+    east = 2,
+    south = 3,
+    west =4
+}
+
+[ExecuteInEditMode]
+public class Tile : MonoBehaviour
+{
+    public Tile(direction CreatedFrom)
+    {
+        if(CreatedFrom == 0)
+        {
+
+
+            return;
+        } 
+
+
+    }
+
+    SideNeighbour[] neighbour;
+
+
+   
+
+
+
+
+
+
+
+
+
+    public void CreateTile()
+    {
+
+    }
+
+}
+
+public class SideNeighbour
+{
+    enum direction
+    {
+        north,
+        east,
+        south,
+        west
+    }
+    public bool ocuppied;
+}
+
